2fb0c0df6fecf777d8680edc2387311aafe9427f
mago <markus.schneider84@gmail.com>
2020-01-14 13:06:05 +0100

    huge pixel day

--- src/Visuals/Rooms/Staircase.gd
+++ src/Visuals/Rooms/Staircase.gd
@@ -8,19 +8,23 @@ func _ready():
 func _on_StairWayCellar_body_entered(body):
 	if body.is_in_group("player"):
 		$StairWayCellar/Label.show()
+		$StairWayCellar/Sprite.frame = 1
 		body.setTarget(Types.Target.GoUp)
 
 func _on_StairWayCellar_body_exited(body):
 	if body.is_in_group("player"):
 		$StairWayCellar/Label.hide()
+		$StairWayCellar/Sprite.frame = 0
 		body.unsetTarget(Types.Target.GoUp)
 
 func _on_StairWayUpper_body_entered(body):
 	if body.is_in_group("player"):
 		$StairWayUpper/Label.show()
+		$StairWayUpper/Sprite.frame = 1
 		body.setTarget(Types.Target.GoDown)
 
 func _on_StairWayUpper_body_exited(body):
 	if body.is_in_group("player"):
 		$StairWayUpper/Label.hide()
+		$StairWayUpper/Sprite.frame = 0
 		body.unsetTarget(Types.Target.GoDown)
